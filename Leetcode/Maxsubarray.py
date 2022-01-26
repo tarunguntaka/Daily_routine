01df6777262cf3f8e14348d3df0bebaa6562cf7d
Tarun Guntaka <tg3789@rit.edu>
2022-01-25 19:08:51 -0500

    maxsubarray is done but needs improvement in space complexity

---
+++ Leetcode/Maxsubarray.py
@@ -0,0 +1,17 @@
+
+# nums Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+
+#A subarray is a contiguous part of an array
+
+def Maxsubarray(nums):
+    
+    currsum = maxsum = nums[0]
+    for i in nums[1:]:
+        currsum = max(i,currsum+i)
+        maxsum = max(currsum,maxsum)
+    
+    return maxsum
+
+nums = [1,2,-3,4,5,-6,5,4,3,-4]
+
+print(Maxsubarray(nums))
